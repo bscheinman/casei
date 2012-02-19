f5b3386fd2d2ff85ecccb75e541828533a4d2608
Brendon Scheinman <bscheinman@gmail.com>
2012-02-19 17:58:04 -0600

    added models for custom scoring

--- ncaacards/models.py
+++ ncaacards/models.py
@@ -4,12 +4,24 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 class NcaaGame(models.Model):
     name = models.CharField(max_length=50)
     # Storing these in plain text for now
     password = models.CharField(blank=True, null=True, max_length=100)
     starting_shares = models.IntegerField(default=100)
     starting_money = models.IntegerField(default=0)
+
+
+class ScoreType(models.Model):
+    name = models.CharField(max_length=30)
+    default_score = models.IntegerField()
+
+
+class ScoringSetting(models.Model):
+    game = models.ForeignKey(NcaaGame)
+    scoreType = models.ForeignKey(ScoreType)
+    points = models.IntegerField()
 
 
 class UserEntry(models.Model):
@@ -23,17 +35,40 @@ class UserEntry(models.Model):
     def get_score(self):
         points = 0
         for team in self.user_teams:
-            points += team.team.score * team.count
+            points += team.team.get_score() * team.count
         return points
 
 
 class Team(models.Model):
     full_name = models.CharField(max_length=50)
     abbrev_name = models.CharField(max_length=6)
-    score = models.IntegerField(default=0)
 
     def __str__(self):
         return self.abbrev_name
+
+
+class GameTeam(models.Model):
+    game = models.ForeignKey(NcaaGame)
+    team = models.ForeignKey(Team)
+    score = models.IntegerField(default=0)
+
+    # We could pass in the multipliers for the team to the method so we don't need to make that db call for each game
+    def update_score(self):
+        score = 0
+        counts = self.counts
+        multipliers = ScoringSetting.objects.filter(game=self.game)
+        for scoreType in ScoreType.objects.all():
+            count = counts.get(scoreType=scoreType)
+            multiplier = multipliers.get(scoreType=scoreType)
+            score += count * multiplier
+        return score
+        
+
+
+class TeamScoreCount(models.Model):
+    team = models.ForeignKey(Team, related_name='counts')
+    scoreType = models.ForeignKey(ScoreType)
+    count = models.IntegerField(default=0)
 
 
 class UserTeam(models.Model):
@@ -65,6 +100,7 @@ class TradeComponent(models.Model):
 
 admin.site.register(Team)
 admin.site.register(UserTeam)
+admin.site.register(ScoreType)
 
 @receiver(post_save, sender=UserEntry, weak=False)
 def create_user_profile(sender, instance, created, **kwargs):
@@ -72,3 +108,10 @@ def create_user_profile(sender, instance, created, **kwargs):
         TradingBlock.objects.create(entry=instance)
         for team in Team.objects.all():
             UserTeam.objects.create(entry=instance, team=team, count=instance.game.starting_shares)
+
+
+# Whenever a team's wins are updated, update the score for that team
+@receiver(post_save, sender=TeamScoreCount, weak=False)
+def update_team_scores(sender, instance, create, **kwargs):
+    for team in GameTeam.objects.filter(team=instance.team):
+        team.update_score()
