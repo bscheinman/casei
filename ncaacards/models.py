edcbdf93bb896dab3594592d2868b6f9ca81811b
Brendon Scheinman <brendon@caseinsensitive.org>
2012-02-19 11:46:30 -0600

    added admin registration

--- ncaacards/models.py
+++ ncaacards/models.py
@@ -1,5 +1,8 @@
+from django.contrib import admin
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Team(models.Model):
     full_name = models.CharField(max_length=50)
@@ -14,7 +17,7 @@ class UserTeam(models.Model):
 
 
 class TradingBlock(models.Model):
-    user = models.ForeignKey(User)
+    user = models.OneToOneField(User)
     teams_desired = models.ManyToManyField(Team, related_name='desired_blocks')
     teams_available = models.ManyToManyField(Team, related_name='available_blocks')
 
@@ -33,3 +36,13 @@ class TradeComponent(models.Model):
     team = models.ForeignKey(Team)
     count = models.IntegerField()
     offer = models.ForeignKey(TradeSide)
+
+admin.site.register(Team)
+admin.site.register(UserTeam)
+
+@receiver(post_save, sender=User, weak=False)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        TradingBlock.objects.create(user=instance)
+        for team in Team.objects.all():
+            UserTeam.objects.create(user=instance, team=team, count=STARTING_COUNT)
