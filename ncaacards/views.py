c160347848b26820c76ace88739818bbf031059f
Brendon Scheinman <brendon@caseinsensitive.org>
2012-02-19 11:46:30 -0600

    added admin registration

--- ncaacards/views.py
+++ ncaacards/views.py
@@ -1,1 +1,5 @@
-# Create your views here.
+from casei.ncaacards.models import 
+from casei.views import render_with_request_context
+
+def home(request):
+    return render_with_request_context(request, 'cards_home.html', { })
