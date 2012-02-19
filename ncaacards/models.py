a38258cb4629126ad06f9f305bbd4824ade90b87
Brendon Scheinman <brendon@caseinsensitive.org>
2012-02-19 11:07:37 -0600

    added trade models

--- ncaacards/models.py
+++ ncaacards/models.py
@@ -11,3 +11,25 @@ class UserTeam(models.Model):
     user = models.ForeignKey(User)
     team = models.ForeignKey(Team)
     count = models.IntegerField(default=0)
+
+
+class TradingBlock(models.Model):
+    user = models.ForeignKey(User)
+    teams_desired = models.ManyToManyField(Team, related_name='desired_blocks')
+    teams_available = models.ManyToManyField(Team, related_name='available_blocks')
+
+
+class TradeSide(models.Model):
+    pass
+
+
+class TradeOffer(models.Model):
+    user = models.ForeignKey(User)
+    bid_side = models.OneToOneField(TradeSide, related_name='bid_offer')
+    ask_side = models.OneToOneField(TradeSide, related_name='ask_offer')
+
+
+class TradeComponent(models.Model):
+    team = models.ForeignKey(Team)
+    count = models.IntegerField()
+    offer = models.ForeignKey(TradeSide)
