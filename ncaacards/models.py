47fcff736af797a0665412bc0b477ce36351838b
Brendon Scheinman <brendon@caseinsensitive.org>
2012-02-19 10:33:35 -0600

    created initial migration

--- ncaacards/models.py
+++ ncaacards/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    full_name = models.CharField(max_length=50)
+    abbrev_name = models.CharField(max_length=6)
+    score = models.IntegerField(default=0)
+
+
+class UserTeam(models.Model):
+    user = models.ForeignKey(User)
+    team = models.ForeignKey(Team)
+    count = models.IntegerField(default=0)
--- settings.py
+++ settings.py
@@ -4,17 +4,17 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Brendon Scheinman', 'brendon@caseinsensitive.org'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
+        'ENGINE': 'mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'casei',                      # Or path to database file if using sqlite3.
+        'USER': 'casei',                      # Not used with sqlite3.
+        'PASSWORD': 'casei',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -116,9 +116,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'casei.ncaacards',
+    'south'
 )
 
 # A sample logging configuration. The only tangible logging
