1f9ad4bd51427b6afdff0a7fae433976880e91a5
Brendon Scheinman <bscheinman@gmail.com>
2012-02-19 19:31:07 -0600

    added template to render offers in table form

--- ncaacards/views.py
+++ ncaacards/views.py
@@ -1,4 +1,28 @@
+from casei.ncaacards.models import *
 from casei.views import render_with_request_context
+from django.db.models import Q
+from django.http import HttpResponseRedirect
 
 def home(request):
     return render_with_request_context(request, 'cards_home.html', { })
+
+
+def marketplace(request, game_id):
+    try:
+        game = NcaaGame.objects.get(id=game_id)
+    except NcaaGame.DoesNotExist:
+        return HttpResponseRedirect('/ncaa/')
+    
+    offers_query = Q(entry__game=game)
+
+    ask_filter = request.GET.get('ask_filter', '')
+    bid_filter = request.GET.get('bid_filter', '')
+
+    if ask_filter:
+        offers_query = offers_query & Q(ask_side__components__team__abbrev_name__iexact=ask_filter)
+    if bid_filter:
+        offers_query = offers_query & Q(bid_side__components__team__abbrev_name__iexact=bid_filter)
+
+    offers = TradeOffer.objects.filter(offers_query)
+
+    return render_with_request_context(request, 'marketplace.html', { 'game'=game, 'offers'=offers })
