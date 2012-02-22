6ecb7dafcd8407582e06fbff1b5ec1faf28ff553
Brendon Scheinman <bscheinman@gmail.com>
2012-02-21 18:35:25 -0600

    basic view refactor checkpoint

---
+++ profiles/models.py
@@ -0,0 +1,22 @@
+from casei.fields import UUIDField
+from datetime import datetime
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='profile')
+    join_date = models.DateTimeField(auto_now_add=True)
+    verification_id = UUIDField(auto=True)
+    is_verified = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.user.username
+
+
+# Automatically create a profile for each user that is created
+@receiver(post_save, sender=User, weak=False)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
