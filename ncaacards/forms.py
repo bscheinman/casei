448842590cd1a2c0af5d84d7504d9f65fd1fd879
Brendon Scheinman <bscheinman@gmail.com>
2012-02-28 21:18:40 -0600

    Trades are now added but do not execute against each other.  Also bid/ask/(presumably last if executions worked) are displayed on the team page although this needs to be cleaned up.

--- ncaacards/forms.py
+++ ncaacards/forms.py
@@ -20,7 +20,7 @@ class TradeForm(forms.Form):
             del cleaned_data['side']
 
         if price:
-            price_pattern = r'\d+(\.\d{2})'
+            price_pattern = r'\d+(\.\d{2})?'
             if not re.search(price_pattern, price):
                 self._errors['price'] = self.error_class(['%s is not a valid price' % price])
                 del cleaned_data['price']
