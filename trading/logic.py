d4903fcdccf3a5ef79e30d13fb089a9c20b25a99
Brendon Scheinman <bscheinman@gmail.com>
2012-02-29 20:44:59 -0600

    Fixed bug in trade logic.  Trades seem to be working mostly correctly now

--- trading/logic.py
+++ trading/logic.py
@@ -1,38 +1,5 @@
 from casei.trading.models import Market, Security, Order, Execution
 
-
-def process_new_order(order):
-    def execute_orders(accepting_order, existing_order):
-        exec_quantity = min([accepting_order.quantity, existing_order.quantity])
-
-        if accepting_order.is_buy:
-            buy_order = accepting_order
-            sell_order = existing_order
-        else:
-            buy_order = existing_order
-            sell_order = accepting_order
-
-        execution = Execution.objects.create(security=order.security, buy_order=buy_order, sell_order=sell_order, quantity=exec_quantity, price=order.price)
-
-    if order.is_buy:
-        comparer = lambda x: x.price <= order.price
-        order_generator = lambda: order.security.get_top_asks()
-    else:
-        comparer = lambda x: x.price >= order.price
-        order_generator = lambda: order.security.get_top_bids()
-
-    while True:
-        orders = order_generator()
-        if not orders:
-            return
-        for matching_order in orders:
-            if not comparer(order):
-                return
-            execute_orders(order, matching_order)
-            if order.quantity_remaining <= 0:
-                return
-
-
 def get_security(market_name, security_name):
     try:
         market = Market.objects.get(name=market_name)
@@ -55,5 +22,3 @@ def place_order(market_name, placer_name, security_name, is_buy, price, quantity
         raise Exception('Offers must have a positive quantity')
 
     order = Order.objects.create(placer=placer_name, security=security, price=price, quantity=quantity, quantity_remaining=quantity, is_buy=is_buy)
-    process_new_order(order)
-    
