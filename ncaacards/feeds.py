894df6f103f927414319035399af4ec2388a6cf9
Brendon Scheinman <bscheinman@gmail.com>
2012-03-13 21:21:56 -0500

    Split RSS feed into two separate feeds for card trades and stock trades

--- ncaacards/feeds.py
+++ ncaacards/feeds.py
@@ -5,7 +5,7 @@ from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from django.utils.feedgenerator import Rss201rev2Feed
 
-class RecentTradesFeed(Feed):
+class RecentStockTradesFeed(Feed):
     feed_type = Rss201rev2Feed
     
     def get_object(self, request, game_id):
@@ -13,7 +13,7 @@ class RecentTradesFeed(Feed):
 
 
     def title(self, game):
-        return 'Recent Trades in %s' % game.name
+        return 'Recent Stock Trades in %s' % game.name
 
 
     def link(self, game):
@@ -26,14 +26,38 @@ class RecentTradesFeed(Feed):
             return 'http://caseinsensitive.org/ncaa/game/%s/team/%s/?start_tab=stock_tab' % (game_team.game.id, game_team.team.abbrev_name)
         except GameTeam.DoesNotExist:
             return 'http://caseinsensitive.org/ncaa/'
-        #return 'http://caseinsensitive.org/ncaa/game/%s/offer/%s/' % (trade.entry.game.id, trade.id)
     
 
     def description(self, game):
-        return 'Trades recently completed in game %s' % game.name
+        return 'Stock trades recently completed in game %s' % game.name
 
 
     def items(self, game):
-        #return TradeOffer.objects.filter(Q(entry__game=game) & ~Q(accepting_user=None)).order_by('-accept_time')[:25]
         return Execution.objects.filter(security__market__name=game.name).order_by('-time')[:50]
 
+
+class RecentCardTradesFeed(Feed):
+    feed_type = Rss201rev2Feed
+    
+    def get_object(self, request, game_id):
+        return get_object_or_404(NcaaGame, pk=game_id)
+
+
+    def title(self, game):
+        return 'Recent Card Trades in %s' % game.name
+
+
+    def link(self, game):
+        return 'http://caseinsensitive.org/ncaa/game/%s/' % game.id
+
+
+    def item_link(self, trade):
+        return 'http://caseinsensitive.org/ncaa/game/%s/offer/%s/' % (trade.entry.game.id, trade.id)
+    
+
+    def description(self, game):
+        return 'Card trades recently completed in game %s' % game.name
+
+
+    def items(self, game):
+        return TradeOffer.objects.filter(Q(entry__game=game) & ~Q(accepting_user=None)).order_by('-accept_time')[:25]
