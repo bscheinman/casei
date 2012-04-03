75a46d3b78c95f591efdc706efba255c7d7be8f7
Brendon Scheinman <brendon@caseinsensitive.org>
2012-04-02 22:24:06 -0500

    Fixed MLB standings scraper to be more flexible wrt whitespace

--- ncaacards/management/commands/update_standings.py
+++ ncaacards/management/commands/update_standings.py
@@ -16,7 +16,7 @@ class Command(NoArgsCommand):
     }
     game_type = GameType.objects.get(name='MLB')
     url = 'http://sports.yahoo.com/mlb/standings'
-    regex = re.compile('<a href="/mlb/teams/(?P<name>[a-z]+)" ?>[A-Za-z ]+</a>\s+</td>\s+<td>\s+(?P<wins>[0-9]+)\s+</td>\s+<td>\s+(?P<losses>[0-9]+)\s+</td>')
+    regex = re.compile('<a href="/mlb/teams/(?P<name>[a-z]+)" ?>[A-Za-z ]+</a>\s*</td>\s*<td>\s*(?P<wins>[0-9]+)\s*</td>\s*<td>\s*(?P<losses>[0-9]+)\s*</td>')
     wins_type, losses_type = ScoreType.objects.get(name='Wins'), ScoreType.objects.get(name='Losses')
 
 
